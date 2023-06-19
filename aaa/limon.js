5fb137d11e4d5425638123a8c9923537c178316b
CandeBS <103084204+CandeBS@users.noreply.github.com>
2023-06-19 18:55:09 -0300

    limon.js

--- aaa/limon.js
+++ aaa/limon.js
@@ -1,12 +1,22 @@
-
 class Limon {
-    constructor() {
-      this.limoncin = loadImage("data/ovalo.png");
-      this.tam = 600;
-    }
-  
-    dibujar() {
-      image(this.limoncin, 0, 0,this.tam, this.tam);
-      
-    }
+  constructor() {
+    this.limoncin = loadImage("data/ovalo.png");
+    this.tam = 600;
+    this.x = (random(20,700));
+    this.y = random(height - this.tam);
+    this.limonAnterior = null;
   }
+
+  dibujar() {
+    image(this.limoncin, this.x, this.y, this.tam, this.tam);
+  }
+
+  reiniciarLimon() {
+    this.x = random(width - this.tam);
+    this.y = random(height - this.tam);
+  }
+
+  generarNuevoLimon() {
+    this.reiniciarLimon(); // Reiniciar la posición del limón
+  }
+} 
