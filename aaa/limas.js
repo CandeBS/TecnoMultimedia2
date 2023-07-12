b2e210fcf887a97a2e0a5f2a1d3015e57fd8cacd
CandeBS <103084204+CandeBS@users.noreply.github.com>
2023-07-12 12:28:07 -0300

    limas.js

--- aaa/limas.js
+++ aaa/limas.js
@@ -1,17 +1,21 @@
-
 class Limas{
     constructor() {
         this.limas = loadImage("data/lima.png");
-        this.tam = random (350,450);
-        this.y = 550;
-        this.x = random(0,600);
-        this.color = int(random(100,200));
+        this.limas2 = loadImage("data/lima2.png");
+        this.tam1 = random (550,1300);
+        this.tam2 = random (550,1000);
+        this.y1 = random (300,550);
+        this.y2 = random (300,550);
+        this.x1 = random(-300,300);
+        this.x2 = random(-300,300);
+        this.color = int(random(50,100));
       }
-    
+
       dibujar() {
         push();
         tint(255,this.color);
-        image(this.limas, this.x, this.y,this.tam, this.tam);
+        image(this.limas, this.x1, this.y1,this.tam1, this.tam1);
+        image(this.limas2, this.x2, this.y2,this.tam2, this.tam2);
         pop();
       }
 }
