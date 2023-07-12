77c6aa45fd359737f5cd075d8eb86d6b06a5d243
CandeBS <103084204+CandeBS@users.noreply.github.com>
2023-07-12 01:36:30 -0300

    limon.js

--- aaa/limon.js
+++ aaa/limon.js
@@ -1,6 +1,6 @@
 class Limon {
   constructor() {
-    this.limoncin = loadImage("data/ovalo.png");
+    this.limoncin = loadImage("data/ovalo2.png");
     this.tam = 700;
     this.x = (random(0,350));
     this.y = random(-300 , 70);
