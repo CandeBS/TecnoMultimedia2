5cba5e00df4e285bcdd1d8fc1d7d88935b2c05fa
CandeBS <103084204+CandeBS@users.noreply.github.com>
2023-07-12 23:41:01 -0300

    limas.js

--- aaa/limas.js
+++ aaa/limas.js
@@ -1,21 +1,22 @@
-class Limas{
-    constructor() {
-        this.limas = loadImage("data/lima.png");
-        this.limas2 = loadImage("data/lima2.png");
-        this.tam1 = random (550,1300);
-        this.tam2 = random (550,1000);
-        this.y1 = random (300,550);
-        this.y2 = random (300,550);
-        this.x1 = random(-300,300);
-        this.x2 = random(-300,300);
-        this.color = int(random(50,100));
-      }
+class Limas {
+  constructor() {
+    this.lima1 = loadImage("data/lima.png");
+    this.lima2 = loadImage("data/lima2.png");
+    this.tam1 = random(550, 1300);
+    this.tam2 = random(550, 1000);
+    this.y1 = random(300, 550);
+    this.y2 = random(300, 550);
+    this.x1 = random(-300, 300);
+    this.x2 = random(-300, 300);
+    this.color = int(random(50, 100));
+  }
 
-      dibujar() {
-        push();
-        tint(255,this.color);
-        image(this.limas, this.x1, this.y1,this.tam1, this.tam1);
-        image(this.limas2, this.x2, this.y2,this.tam2, this.tam2);
-        pop();
-      }
+  dibujar() {
+    limaPGraphics.clear(); // Saca todo lo del PGraphics antes de dibujar las limas
+    limaPGraphics.push();
+    limaPGraphics.tint(255, this.color);
+    limaPGraphics.image(this.lima1, this.x1, this.y1, this.tam1, this.tam1);
+    limaPGraphics.image(this.lima2, this.x2, this.y2, this.tam2, this.tam2);
+    limaPGraphics.pop();
+  }
 }
