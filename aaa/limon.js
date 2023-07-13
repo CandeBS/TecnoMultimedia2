a09d6ae8a5d76324c4e88def4f3850a60d492cf4
CandeBS <103084204+CandeBS@users.noreply.github.com>
2023-07-12 23:42:21 -0300

    limon.js

--- aaa/limon.js
+++ aaa/limon.js
@@ -3,6 +3,7 @@ class Limon {
     this.limones = limones;
     this.limoncin = Math.floor(Math.random() * cantLimones);
     this.tam = 700;
+    //Esto es como lo del arreglo, para que cambien los limones
     this.x = Math.random() * 350;
     this.y = Math.random() * (-300 - 70) + 70;
   }
